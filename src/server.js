7ff42efe77e469c2d71c02729f28b2f6660ba149
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-04-25 18:59:24 +0700

    Implement centralized error handling middleware and ApiError class; update boardController and boardValidation for improved error management

--- src/server.js
+++ src/server.js
@@ -2,8 +2,9 @@
 import express from 'express'
 import AsyncExitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
-import { env } from './config/environment'
+import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
@@ -13,6 +14,9 @@ const START_SERVER = () => {
 
   //Use APIs V1
   app.use('/V1', APIs_V1)
+
+  // Middleware xử lý lỗi tập trung
+  app.use(errorHandlingMiddleware)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(
