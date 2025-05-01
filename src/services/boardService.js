e041ea6220e4288b5a0f3d595bbdb9a66b67711f
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-05-01 09:03:18 +0700

    Add lodash dependency and enhance board model and service with new features
    - Introduced lodash for deep cloning in board service.
    - Updated board model to include 'type' field with validation.
    - Enhanced getDetails method to aggregate columns and cards.
    - Updated board validation to include 'type' validation.

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -3,6 +3,7 @@ import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
 import { StatusCodes } from 'http-status-codes'
+import { cloneDeep } from 'lodash'
 
 const createNew = async (reqBody) => {
   try {
@@ -29,7 +30,24 @@ const getDetails = async (boardId) => {
       throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
     }
 
-    return board
+    const resBoard = cloneDeep(board)
+    //Đưa card về đúng column
+    //method equals được mongoDb support
+    resBoard.columns.forEach((column) => {
+      column.cards = resBoard.cards.filter((card) =>
+        card.columnId.equals(column._id)
+      )
+    })
+
+    // resBoard.columns.forEach((column) => {
+    //   column.cards = resBoard.cards.filter(
+    //     (card) => card.columnId.toString() === column._id.toString()
+    //   )
+    // })
+
+    delete resBoard.cards
+
+    return resBoard
   } catch (error) {
     throw error
   }
