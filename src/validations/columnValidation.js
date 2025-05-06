60ebc1dad5a19537163d69455bee77b5a3074604
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-05-06 09:30:01 +0700

    Implement update functionality for columns with validation and service logic

--- src/validations/columnValidation.js
+++ src/validations/columnValidation.js
@@ -26,6 +26,38 @@ const createNew = async (req, res, next) => {
   }
 }
 
+const update = async (req, res, next) => {
+  // Không dùng required trong trường hợp update
+  const correctCondition = Joi.object({
+    //Nếu làm thêm tính năng di chuyển column sang board khác thì mới validate boardId
+    // boardId: Joi.string()
+    //   .pattern(OBJECT_ID_RULE)
+    //   .message(OBJECT_ID_RULE_MESSAGE),
+    title: Joi.string().min(3).max(50).trim().strict(),
+    cardOrderIds: Joi.array().items(
+      Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+    )
+  })
+
+  try {
+    //set abortEarly: false trong có nhiều lỗi validation thì trả về tất cả lỗi
+    // allowUnknown: true cho phép đẩy các field ngoài các field đã định nghĩa trên correctCondition
+    await correctCondition.validateAsync(req.body, {
+      abortEarly: false,
+      allowUnknown: true
+    })
+    next()
+  } catch (error) {
+    const errorMessage = new Error(error).message
+    const customError = new ApiError(
+      StatusCodes.UNPROCESSABLE_ENTITY,
+      errorMessage
+    )
+    next(customError)
+  }
+}
+
 export const columnValidation = {
-  createNew
+  createNew,
+  update
 }
