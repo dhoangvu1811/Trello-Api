e44c03c9879e81bf73ad0260edd4e569848ef6bd
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-06-04 20:04:11 +0700

    feat: enhance card update functionality to support file uploads and user associations

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -6,6 +6,7 @@ import { BOARD_TYPE } from '~/utils/constants'
 import { columnModel } from '~/models/columnModel'
 import { cardModel } from '~/models/cardModel'
 import { pagingSkipValue } from '~/utils/algorithms'
+import { userModel } from './userModel'
 
 //Define Collection (Name & Schema)
 const BOARD_COLLECTION_NAME = 'boards'
@@ -104,6 +105,26 @@ const getDetails = async (userId, boardId) => {
             localField: '_id',
             foreignField: 'boardId',
             as: 'cards'
+          }
+        },
+        {
+          $lookup: {
+            from: userModel.USER_COLLECTION_NAME,
+            localField: 'ownerIds',
+            foreignField: '_id',
+            as: 'owners',
+            // pipeline trong lookup là để xử lý một hoặc nhiều luồng cần thiết
+            // $project để chỉ định vài field không muốn lấy về bằng cách gán nó giá trị bằng 0
+            pipeline: [{ $project: { password: 0, verifyToken: 0 } }]
+          }
+        },
+        {
+          $lookup: {
+            from: userModel.USER_COLLECTION_NAME,
+            localField: 'memberIds',
+            foreignField: '_id',
+            as: 'members',
+            pipeline: [{ $project: { password: 0, verifyToken: 0 } }]
           }
         }
       ])
